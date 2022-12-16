025815268eca5b5c996a3c849e59589cdf0187ec
yangpeixuan <yangpeixuan@aecg.com>
2022-12-16 18:07:22 +0800

    init

---
+++ lib/file_operate.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FileOperateRoute extends StatefulWidget {
+  FileOperateRoute({Key? key}) : super(key: key);
+
+  @override
+  _FileOperateRouteState createState() => new _FileOperateRouteState();
+}
+
+class _FileOperateRouteState extends State<FileOperateRoute> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    //从文件读取点击次数
+    _readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _getLocalFile() async {
+    //获取应用目录
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    return new File('$dir/counter.txt');
+  }
+
+  Future<int> _readCounter() async {
+    try {
+      File file = await _getLocalFile();
+      //读取点击次数（以字符串）
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } on FileSystemException {
+      return 0;
+    }
+  }
+
+  Future<Null> _incrementCounter() async {
+    setState(() {
+      _counter++;
+    });
+    //将点击文件写入文件
+    await (await _getLocalFile()).writeAsString('$_counter');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('文件操作')),
+      body: new Center(
+        child: new Text('点击了 $_counter 次'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
